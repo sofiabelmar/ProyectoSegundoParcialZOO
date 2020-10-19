dd33554b4461b68c788bfc90eeb84d45371c2277
Alumno <ulsa@D113.local>
2020-10-19 14:32:09 -0700

    zoologico

---
+++ ProyectoSegundoParcial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoSegundoParcial
+//
+//  Created by Alumno on 10/15/20.
+//  Copyright © 2020 Estudiate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProyectoSegundoParcial/menuController.swift
@@ -0,0 +1,20 @@
+//
+//  menuController.swift
+//  ProyectoSegundoParcial
+//
+//  Created by Alumno on 10/19/20.
+//  Copyright © 2020 Estudiate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class menuController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+ 
+    @IBAction func doTapSalir(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
